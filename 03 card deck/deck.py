357f8e2879cb335f0976b4a679fc36720975e79b
RosenRitter-Mike <mike88roze@gmail.com>
2025-03-22 18:06:57 +0200

    exam_home_deck_notes_added

--- 03 card deck/deck.py
+++ 03 card deck/deck.py
@@ -25,7 +25,17 @@ def fair_deck(func):
 
 
 class Deck(IDeck):
+    """
+    A representing the standard card deck of playing cards
+    """
+
     def __init__(self, shuffle=True):
+        """
+        Initialize a new deck shuffled by default.
+
+        Args:
+            shuffle: True - deck is shuffled, False - deck as is.
+        """
         self._cards = Deck.build_deck()
         self._index = 0
         if shuffle:
@@ -33,11 +43,23 @@ class Deck(IDeck):
 
     @property
     def cards(self):
+        """
+        get the cards list of the deck
+
+        Returns:
+             cards: a list of Card objects
+        """
         return self._cards
 
     @override
     @fair_deck
     def shuffle(self):
+        """
+        Shuffles the deck of cards that is the card objects list
+
+        Returns:
+             cards: a list of Card objects
+        """
         # rnd.shuffle(self._cards)
         new_deck = rnd.sample(self._cards, len(self._cards))
         self._cards = new_deck
@@ -45,18 +67,50 @@ class Deck(IDeck):
 
     @override
     def draw(self) -> Card:
-        return self._cards.pop()
+        """
+        Draw the card on the top of the deck of cards removing it from the deck
+
+        Returns:
+             card: a Card object that was on the top of the deck.
+        """
+
+        return self._cards.pop() if self._cards else None
 
     @override
     def add_card(self, new_card: Card):
+        """
+        Add a card to the bottom of the deck
+
+        Args:
+            new_card: a Card object that is to be returned to the deck
+
+        Returns:
+             cards: a list of Card objects with the added Card
+
+        """
         self._cards.append(new_card)
 
     @override
     def __len__(self):
+        """
+        Returns the amount of cards in the deck
+
+        Returns:
+            int: the number of cards currently in the deck
+        """
         return len(self._cards)
 
     @override
     def __getitem__(self, idx):
+        """
+        Returns the Card at the index idx
+
+        Args:
+            idx: the index of the card wanted
+
+        Returns:
+             Card: the Card object at the index given
+        """
         if not isinstance(idx, int):
             raise TypeError("index of card can only be int")
         return self._cards[idx]
@@ -76,6 +130,12 @@ class Deck(IDeck):
 
     @classmethod
     def build_deck(cls) -> list[Card]:
+        """
+        a utility function to create the deck
+
+        Returns:
+             Deck: a standard card deck not shuffled
+        """
         new_deck = []
         for suit in CardSuit:
             for rank in CardRank:
